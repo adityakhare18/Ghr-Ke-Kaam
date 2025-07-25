b3fdead5cc5247bbb3ada1cc8419cf2bfa2bd8ab
adityakhare18 <khareaditya0105@gmail.com>
2025-07-25 22:08:28 +0530

    Remove unused booking controller methods from bookings route

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authenticateToken, requireCustomer } from '../middlewares/auth.js';
-import { showCreateForm, createBooking, getBookings, acceptBooking, rejectBooking } from '../controllers/bookingController.js';
+import { showCreateForm, createBooking, getBookings  } from '../controllers/bookingController.js';
 
 const router = express.Router();
 
